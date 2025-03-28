130a6d1f5237c65532f19ae19eb20bfca579f00c
Sabnock <kingsabnock@gmail.com>
2025-03-28 15:57:13 +0800

    Add fade-in animation to homepage and remove obsolete SQL dump file

--- pages/users/homepage.php
+++ pages/users/homepage.php
@@ -60,8 +60,18 @@ $announcements = mysqli_fetch_all($result, MYSQLI_ASSOC);
         
         body {
             font-family: 'Inter', sans-serif;
+            animation: fadeIn 1s ease-in-out;
         }
-        
+
+        @keyframes fadeIn {
+            from {
+                opacity: 0;
+            }
+            to {
+                opacity: 1;
+            }
+        }
+
         .heading-font {
             font-family: 'Montserrat', sans-serif;
         }
